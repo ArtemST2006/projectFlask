6fb29eb9d754e67a7d8aa1fc40a12739be1255a2
Администратор <arstep2006@gmail.com>
2024-04-22 17:47:43 +0300

    lastдфыеism

--- data/api_test.py
+++ data/api_test.py
@@ -1,6 +1,6 @@
 from requests import get, post, delete
 import pprint
 
-pprint.pprint(get('http://127.0.0.1:8091/api/place/3').json())
-pprint.pprint(get('http://127.0.0.1:8091/api/place').json())
+pprint.pprint(get('http://127.0.0.1:8092/api/place/3').json())
+pprint.pprint(get('http://127.0.0.1:8092/api/place').json())
 
